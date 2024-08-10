ea90ae8df7db97a531d4d96d15c430a5640891bb
Douglas <133309975+douglas2570@users.noreply.github.com>
2024-08-10 17:04:17 -0300

    update script.js

--- Reviewing the OOP/Composition/00-User-Profile/src/script.js
+++ Reviewing the OOP/Composition/00-User-Profile/src/script.js
@@ -27,38 +27,35 @@ class User {
         return this.#profiles;
     }
 
+    updateProfileList(profileList) {
+        profileList = document.getElementById(profileList);
+        profileList.innerHTML = ''; 
+
+        this.getProfiles().forEach(profile => {
+            const row = document.createElement('tr');
+            const cell = document.createElement('td');
+            cell.textContent = profile.getRole();
+            row.appendChild(cell);
+            profileList.appendChild(row);
+        });
+}
+
     getName() {
         return this.#name;
     }
 }
 
-
 const user = new User('Alice');
-
 
 document.getElementById('userName').textContent = user.getName();
 
-
 document.getElementById('addProfileBtn').addEventListener('click', function () {
-    const profileType = document.getElementById('profileType').value;
+    let profileType = document.getElementById('profileType').value;
     if (profileType) {
         const newProfile = new Profile(profileType);
         user.addProfile(newProfile);
-        updateProfileList();
-        document.getElementById('profileType').value = ''; 
+        
+        user.updateProfileList('profileList');
+        profileType = ''; 
     }
 });
-
-
-function updateProfileList() {
-    const profileList = document.getElementById('profileList');
-    profileList.innerHTML = ''; 
-
-    user.getProfiles().forEach(profile => {
-        const row = document.createElement('tr');
-        const cell = document.createElement('td');
-        cell.textContent = profile.getRole();
-        row.appendChild(cell);
-        profileList.appendChild(row);
-    });
-}
